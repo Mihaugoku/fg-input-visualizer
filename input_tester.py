fa087fc42b9241591e42977c85d61e26864ee68e
Mihaugoku <59739893+Mihaugoku@users.noreply.github.com>
2025-02-02 22:55:33 +0100

    Some fixes and stuff

--- input_tester.py
+++ input_tester.py
@@ -1,14 +1,22 @@
 from pynput.keyboard import Listener
 
-import asyncio
-import gamepad_reader
+import gamepad_tester
+import threading
 
 
 def check_key_press(key):
-    print(str(key).replace("'", ""))
+    key = str(key).replace("'", "")
+    print(key)
+    if key.strip() == "Key.esc":
+        listener.stop()
+        exit(0)
 
 
-print("Listening for keypresses... Press Ctrl+C to quit.")
+print("Listening for keypresses... Press Escape or Ctrl+C to quit.")
+
+
+gamepad_thread = threading.Thread(target=gamepad_tester.gamepad_tester_main, daemon=True)
+gamepad_thread.start()
 
 
 listener = Listener(on_press=check_key_press)
